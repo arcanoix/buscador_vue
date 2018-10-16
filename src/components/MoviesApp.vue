e72e4695227f737b87e00fc9f6f114fee2eb2460
thavoo <gustavoh.2312@gmail.com>
2018-10-15 21:05:20 -0500

    upload success finally process search movie with vuex

---
+++ src/components/MoviesApp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="text-center text-muted">Buscador Con Vuex</h1>
+        <search-form></search-form>
+        <hr>
+        <movie-list></movie-list>
+    </div>
+</template>
+
+
+<script>
+
+    import SearchForm from './SearchForm.vue'
+    import MovieList from './MovieList.vue';
+
+export default {
+    components:{
+        SearchForm,
+        MovieList
+    }
+}
+</script>
+
